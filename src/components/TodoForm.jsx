1f3feb76e9b59d6e43fbe17450b52397b2cf6ebb
Angel Astorga <astorga_ms@hotmail.com>
2025-07-22 18:26:41 -0400

    feat: Created the components TodoForm and TodoList. refactor: Updated the App.jsx to use the new components

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,10 @@
+function TodoForm() {
+  return (
+    <form action="">
+      <label htmlFor="todoTile"> Todo</label>
+      <input id="todoTItle" type="text" />
+      <button> Add Todo</button>
+    </form>
+  );
+}
+export default TodoForm;
